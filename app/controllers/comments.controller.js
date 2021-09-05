f6307044c6ef59e947df836e33585504b2ee5022
Guilherme Oliveira <coliveira.guilherme@gmail.com>
2021-09-05 18:57:09 -0300

    add more error handlers

--- app/controllers/comments.controller.js
+++ app/controllers/comments.controller.js
@@ -12,7 +12,7 @@ module.exports = {
 		try {
 			comments = await DB.Comments.findAll({order: [['createdAt', 'DESC']]});
 		} catch {
-			return res.status(400).json({ error: 'Erro ao consultar dados' });
+			return res.status(500).json({ error: 'Erro ao consultar dados do servidor' });
 		}
 
 		return res.status(200).json(comments);
@@ -21,20 +21,20 @@ module.exports = {
 	async save(req, res) {
 		const comment_text = req.body.data;
 		const comment_hash = helper.hashCode(comment_text);
+	
+		let ret;
+		try {
+			ret = await DB.Comments.create({comment: comment_text, hash:comment_hash});
+		} catch(error) {
+			return res.status(500).json({ error: 'Erro ao salvar dados' });
+		}
 
 		if( !helper.soundExists(comment_hash) ) {
 			try {
 	            response = await TTS.getAudio(comment_text, comment_hash);
 	        } catch (error) {
-	            console.log(error);
+	            return res.status(500).json({ error: 'Erro ao consultar dados da API' });
 	        }
-		}
-		
-		let ret;
-		try {
-			ret = await DB.Comments.create({comment: comment_text, hash:comment_hash});
-		} catch(error) {
-			console.log(error);
 		}
 
 		return res.status(200).json({data: ret.dataValues});
